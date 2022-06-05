b80b1c65d71694d4e7bc7feb659400e24b4636cb
Twarit Waikar <wtwarit@gmail.com>
2022-06-06 02:22:19 +0530

    Add framebuffers

--- engine/platform/vulkan/vulkan_renderer.cpp
+++ engine/platform/vulkan/vulkan_renderer.cpp
@@ -18,6 +18,41 @@
 
 VulkanRenderer::VulkanRenderer(const char* app_name, const GLFWWindow* window)
 {
+	build_vulkan_contexts(app_name, window);
+	build_swapchain(window->get_width(), window->get_height());
+	build_queue_and_command_buffers();
+	build_default_render_pass();
+	build_framebuffers();
+
+	is_ok = true;
+}
+
+VulkanRenderer::~VulkanRenderer()
+{
+	if (!is_ok)
+	{
+		return;
+	}
+
+	vkDestroyCommandPool(device, graphics_queue_command_pool, nullptr);
+	vkDestroySwapchainKHR(device, swapchain, nullptr);
+
+	vkDestroyRenderPass(device, render_pass, nullptr);
+
+	for (int i = 0; i < swapchain_image_views.size(); i++)
+	{
+		vkDestroyFramebuffer(device, framebuffers[i], nullptr);
+		vkDestroyImageView(device, swapchain_image_views[i], nullptr);
+	}
+
+	vkDestroyDevice(device, nullptr);
+	vkDestroySurfaceKHR(instance, surface, nullptr);
+	vkb::destroy_debug_utils_messenger(instance, debug_messenger);
+	vkDestroyInstance(instance, nullptr);
+}
+
+void VulkanRenderer::build_vulkan_contexts(const char* app_name, const GLFWWindow* window)
+{
 	vkb::detail::Result<vkb::Instance> builder_instance
 	    = vkb::InstanceBuilder()
 	          .set_app_name(app_name)
@@ -48,32 +83,6 @@ VulkanRenderer::VulkanRenderer(const char* app_name, const GLFWWindow* window)
 
 	graphics_queue_family = vkb_device.get_queue_index(vkb::QueueType::graphics).value();
 	graphics_queue = vkb_device.get_queue(vkb::QueueType::graphics).value();
-
-	build_swapchain(window->get_width(), window->get_height());
-	build_queue_and_command_buffers();
-
-	is_ok = true;
-}
-
-VulkanRenderer::~VulkanRenderer()
-{
-	if (!is_ok)
-	{
-		return;
-	}
-
-	vkDestroyCommandPool(device, graphics_queue_command_pool, nullptr);
-	vkDestroySwapchainKHR(device, swapchain, nullptr);
-
-	for (VkImageView& v : swapchain_image_views)
-	{
-		vkDestroyImageView(device, v, nullptr);
-	}
-
-	vkDestroyDevice(device, nullptr);
-	vkDestroySurfaceKHR(instance, surface, nullptr);
-	vkb::destroy_debug_utils_messenger(instance, debug_messenger);
-	vkDestroyInstance(instance, nullptr);
 }
 
 void VulkanRenderer::build_swapchain(uint32_t width, uint32_t height)
@@ -87,6 +96,8 @@ void VulkanRenderer::build_swapchain(uint32_t width, uint32_t height)
 	          .value();
 
 	swapchain = vkb_swapchain.swapchain;
+	swapchain_image_width = width;
+	swapchain_image_height = height;
 	swapchain_images = vkb_swapchain.get_images().value();
 	swapchain_image_views = vkb_swapchain.get_image_views().value();
 	swapchain_image_format = vkb_swapchain.image_format;
@@ -106,4 +117,67 @@ void VulkanRenderer::build_queue_and_command_buffers()
 	    device,
 	    &cmd_buffer_info,
 	    &graphics_queue_command_buffer));
+}
+
+void VulkanRenderer::build_default_render_pass()
+{
+	// Image lifetime -
+	//
+	// UNDEFINED
+	//   RenderPass Begins
+	//     Subpass 0 begins(Transition to Attachment Optimal)
+	//     Subpass 0 renders
+	//     Subpass 0 ends
+	//   Renderpass Ends(Transitions to Present Source)
+
+	VkAttachmentDescription color_attachment = {};
+	color_attachment.format = swapchain_image_format;
+	color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+	color_attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+	color_attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+	color_attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+
+	color_attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+	color_attachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+	VkAttachmentReference color_attachment_ref = {};
+	color_attachment_ref.attachment = 0;
+	color_attachment_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+	VkSubpassDescription subpass = {};
+	subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+	subpass.colorAttachmentCount = 1;
+	subpass.pColorAttachments = &color_attachment_ref;
+
+	VkRenderPassCreateInfo render_pass_info = {};
+	render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+
+	render_pass_info.attachmentCount = 1;
+	render_pass_info.pAttachments = &color_attachment;
+	render_pass_info.subpassCount = 1;
+	render_pass_info.pSubpasses = &subpass;
+
+	VK_CHECK(vkCreateRenderPass(device, &render_pass_info, nullptr, &render_pass));
+}
+
+void VulkanRenderer::build_framebuffers()
+{
+	VkFramebufferCreateInfo fb_info = {};
+	fb_info.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+	fb_info.pNext = nullptr;
+
+	fb_info.renderPass = render_pass;
+	fb_info.attachmentCount = 1;
+	fb_info.width = swapchain_image_width;
+	fb_info.height = swapchain_image_height;
+	fb_info.layers = 1;
+
+	framebuffers = Vector<VkFramebuffer>(swapchain_images.size());
+
+	for (int i = 0; i < swapchain_images.size(); i++)
+	{
+		fb_info.pAttachments = &swapchain_image_views[i];
+		VK_CHECK(vkCreateFramebuffer(device, &fb_info, nullptr, &framebuffers[i]));
+	}
 }
--- engine/platform/vulkan/vulkan_renderer.h
+++ engine/platform/vulkan/vulkan_renderer.h
@@ -13,8 +13,11 @@ public:
 	~VulkanRenderer();
 
 private:
+	void build_vulkan_contexts(const char* app_name, const GLFWWindow* window);
 	void build_swapchain(uint32_t width, uint32_t height);
 	void build_queue_and_command_buffers();
+	void build_default_render_pass();
+	void build_framebuffers();
 
 	// Context variables
 	bool is_ok = false;
@@ -29,10 +32,18 @@ private:
 	VkFormat swapchain_image_format;
 	Vector<VkImage> swapchain_images;
 	Vector<VkImageView> swapchain_image_views;
+	uint32_t swapchain_image_width;
+	uint32_t swapchain_image_height;
 
 	// Queue handles
 	VkQueue graphics_queue;
 	uint32_t graphics_queue_family;
+
+	// Command pools/buffers
 	VkCommandPool graphics_queue_command_pool;
 	VkCommandBuffer graphics_queue_command_buffer;
+
+	// Renderpass objects
+	VkRenderPass render_pass;
+	Vector<VkFramebuffer> framebuffers;
 };
